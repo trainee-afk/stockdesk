75433a3926ca9c354d998ee964f003a61aeba84e
Meet Patel <meet@acedataanalytics.com>
2026-09-04 10:28:51 +0530

    fix: db error

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-const { connect } = require('../routes/authRoute');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
